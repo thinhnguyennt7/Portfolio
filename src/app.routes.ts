5a0e998162ab320c96cb5350a8665bc43c592969
tnguyen452 <tnntech@gatech.edu>
2018-03-22 16:32:39 -0400

    routes

---
+++ src/app.routes.ts
@@ -0,0 +1,10 @@
+// import { HeaderComponent } from "./header/header.component";
+// import { provideRouter } from "@angular/router";
+
+// const APP_ROUTES = [
+// 	{ path: 'header', component: HeaderComponent }
+// ];
+
+// export const APP_ROUTES_PROVIDER = [
+// 	provideRouter(APP_ROUTES)
+// ];
